58c34ea7fa66b547bf63554a3b1d9cfe1e553581
marsCat <marscatcn@live.com>
2019-03-24 21:50:01 +0800

    更新

--- libethcore/TransactionBase.cpp
+++ libethcore/TransactionBase.cpp
@@ -31,7 +31,7 @@
 using namespace std;
 using namespace dev;
 using namespace dev::eth;
-
+using namespace msk;
 
 /*
 * Maskash marsCatXdu
@@ -50,6 +50,27 @@ TransactionBase::TransactionBase(TransactionSkeleton const& _ts, Secret const& _
 	m_sender(_ts.from)
 {
 	msk::isSnarkOk();
+	ppT::init_public_params();
+    inhibit_profiling_info = true;
+    inhibit_profiling_counters = true;
+
+    uint256 ask_s=uint256S("038cce42abd366b83ede7e009130de5372cdf73dee8251148cb48d1b9af68ad0");
+   
+    uint256 apk_r=uint256S("038cce42abd366b83ede7e009130de5372cdf73dee8251148cb48d1b9af68ad1");
+
+    uint64_t v_1=5;
+    uint64_t v_2=3;
+    uint64_t v_3=0;
+
+    uint256 old_r=uint256S("038cce42abd366b83ede8e009130de5372cdf73dee2251148cb48d1b4af68a45");
+
+    uint256 new_r1=uint256S("038cce42abd366b83ede9e009130de5372cdf73dee3251148cb48d1b5af68ad0");
+    uint256 new_r2=uint256S("038cce42abd366b83ede9e009130de5372cdf73dee3251148cb48d1b5af68ad0");
+
+    transferZero tr= makeTransferZero<FieldT>( apk_r, new_r1,new_r2,v_1,ask_s,old_r,v_2);
+    bool t=transferZeroVerify<FieldT>(tr.SNold ,tr.krnew,tr.ksnew, tr.data, tr.pi,tr.vk,tr.c_rt,tr.s_rt,tr.r_rt);
+    cout<<t<<endl;
+
 	if (_s)
 		sign(_s);
 }
